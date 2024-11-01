db498747eebb531a34d5f909112bf647fa0f1daf
JaViKo500 <javikogutierrez64@gmail.com>
2024-10-31 23:50:23 -0500

    check imports

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
 
 @Module({
